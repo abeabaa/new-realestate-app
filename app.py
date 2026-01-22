7b1ad136d517ea397a3ef6e065cdb3d42313e13b
abeabaa <jaewook0412a@gmail.com>
2026-01-22 12:52:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -107,9 +107,9 @@ else:
             xref="x", yref="y",
             axref="x", ayref="y",
             showarrow=True,
-            arrowhead=2, # 화살표 머리 모양 (1~8)
-            arrowsize=1.5,
-            arrowwidth=2,
+            arrowhead=5, # 화살표 머리 모양 (1~8)
+            arrowsize=3,
+            arrowwidth=5,
             arrowcolor=color_map.get(region, "black")
         )
 
@@ -133,3 +133,4 @@ else:
     )
 
     st.plotly_chart(fig, use_container_width=True)
+
